70e28f9cea9470aa69928fe1320f7d7234b87a32
Michał Ordon <michal.ordonv1@gmail.com>
2023-09-21 21:55:54 +0200

    Pushig current files to github

---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Ja',
+        'title': 'Blog Post 1',
+        'content': 'First post comment',
+        'date_posted': 'August 27, 2023'
+    },
+    {
+        'author': 'Ty',
+        'title': 'Blog Post 2',
+        'content': 'Second post comment',
+        'date_posted': 'August 28, 2023'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'about'})
